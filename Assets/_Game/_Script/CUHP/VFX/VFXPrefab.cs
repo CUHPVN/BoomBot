f98176f530720240d64075f57cd15c181ad6f752
CUHP_VN <phuc260706@gmail.com>
2025-09-18 00:08:30 +0700

    add vfx

--- Assets/_Game/_Script/CUHP/VFX/VFXPrefab.cs
+++ Assets/_Game/_Script/CUHP/VFX/VFXPrefab.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class VFXPrefab : GameUnit
 {
+    [SerializeField] private GameObject[] texts;
     private void OnEnable()
     {
+        ChooseOne();
         Invoke(nameof(Despawn), 2f);
+    }
+    private void ChooseOne()
+    {
+        for(int i=0; i<texts.Length; i++)
+        {
+            texts[i].SetActive(false);
+        }
+        int ran = Random.Range(0, texts.Length);
+        texts[ran].SetActive(true);
     }
     private void Despawn()
     {
