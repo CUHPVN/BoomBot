c72db24a8b24d33852dfd99dd8a22ff528b62132
dongchoido <dong7cnc@gmail.com>
2025-09-20 17:17:32 +0700

    xoay nhân vật

--- Assets/_Game/_Script/DONG/PlayerController.cs
+++ Assets/_Game/_Script/DONG/PlayerController.cs
@@ -9,16 +9,24 @@ public class PlayerController : MonoBehaviour
     public CameraBound cameraBound;
     public InputManager inputManager;
     public RopeSwing ropeSwing;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
     public void Init()
     {
         inputManager = FindFirstObjectByType<InputManager>();
         cameraBound = FindFirstObjectByType<CameraBound>();
     }
 
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
-        
+        if (rb.velocity.x > 0.01f) sr.flipX = false;
+        if (rb.velocity.x < -0.01f) sr.flipX = true;
     }
 
     private void OnTriggerEnter2D(Collider2D col)
